12f58e94e35700325becad6d99cd62ddfcc9d6d4
TheRoyalBlock <turbinenetwork@gmail.com>
2017-05-09 21:05:06 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace TheRoyalBlock\PluginName
+  
+//Blocks
+use pocketmine\block\Block;
+
+//Command
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+//Entity
+use pocketmine\entity\Entity;
+use pocketmine\entity\Effect;
+
+//Events
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent; 
+
+//Inventory
+use pocketmine\inventory\ChestInventory;
+use pocketmine\inventory\EnderChestInventory;
+
+//Item
+use pocketmine\item\Item;
+
+//Lang
+
+//Level
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+
+//Math
+use pocketmine\math\Vector3;
+
+//Metadata
+
+//Nbt
+use pocketmine\nbt\NBT;
+
+//Network
+use pocketmine\network\Network;
+
+//Permission
+use pocketmine\permission\Permission;
+
+//Plugin
+use pocketmine\plugin\PluginBase;
+
+//Scheduler
+use pocketmine\scheduler\PluginTask;
+
+//Tile
+use pocketmine\tile\Sign;
+use pocketmine\tile\Chest;
+
+//Utils
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+
+//Other
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+    
+  const PREFIX = C::GOLD . "[" . C::BLUE . "PluginName" . C::GOLD . "] ". C::RESET . C::WHITE;
+      public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(self::PREFIX . "Plugin loaded!");
+    }
+      public function onDisable(){
+        $this->getLogger()->info(self::PREFIX . "Plugin disabled!");
+    }
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+  switch($cmd->getName()){
+    case "":
+    
