eb93e42f198277b090e561f80e5fe6055cdb7d2a
TheRoyalBlock <TheRoyalBlock.MC@gmail.com>
2017-12-15 17:25:54 -0500

    Create EventListener.php

---
+++ Plugin base/src/TheRoyalBlock/PluginName/EventListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TheRoyalBlock\PluginName;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerAchievementAwardedEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+
+class EventListener implements Listener {
+
+// Good enough for you?
+}
+?>
